5b84d92025d82607c323011f5c2d91084f67900e
Robert Casey <rcasey212@gmail.com>
2017-01-18 15:07:19 -0600

    Removed code. Updated README to point to the new repo location under Morningstar.

--- src/main/scala/com/quantifind/kafka/offsetapp/OffsetInfoReporter.scala
+++
@@ -1,8 +0,0 @@
-package com.quantifind.kafka.offsetapp
-
-import com.quantifind.kafka.OffsetGetter.OffsetInfo
-
-trait OffsetInfoReporter {
-  def report(info: IndexedSeq[OffsetInfo])
-  def cleanupOldData() = {}
-}
